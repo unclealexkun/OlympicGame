8456a35a31ebebe129debaff9024710b7ad511a6
UncleAlexKun <raisin27@mail.ru>
2019-10-29 21:45:38 +0400

    Решение задачи C в Яндекс.Интервью

--- SolutionOlimpicGame/SolutionOlimpicGame/Program.cs
+++ SolutionOlimpicGame/SolutionOlimpicGame/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SolutionOlimpicGame
 {
@@ -10,6 +7,35 @@ namespace SolutionOlimpicGame
 	{
 		static void Main(string[] args)
 		{
+			var line = Console.ReadLine();
+			if (line != null)
+			{
+				var list = new List<int>();
+				var size = Int32.Parse(line);
+				if (size > 0)
+				{
+					line = Console.ReadLine();
+
+					var currentElement = Int32.Parse(String.IsNullOrEmpty(line) ? "0" : line);
+					list.Add(currentElement);
+
+					for (int i = 1; i < size; i++)
+					{
+						line = Console.ReadLine();
+						var element = Int32.Parse(String.IsNullOrEmpty(line) ? "0" : line);
+						if (element != currentElement)
+						{
+							list.Add(element);
+							currentElement = element;
+						}
+					}
+
+					foreach (var element in list)
+					{
+						Console.WriteLine(element);
+					}
+				}
+			}
 		}
 	}
 }
--- SolutionOlimpicGame/SolutionOlimpicGamePython/SolutionOlimpicGamePython.py
+++ SolutionOlimpicGame/SolutionOlimpicGamePython/SolutionOlimpicGamePython.py
@@ -1,1 +1,13 @@
+size = int(input())
+if size > 0:
+    currentElement = int(input())
+    newList = [currentElement]
 
+    for index in range(1, size):
+        element = int(input())
+        if currentElement != element:
+            newList.append(element)
+            currentElement = element
+        
+    for element in newList:
+        print(element)
