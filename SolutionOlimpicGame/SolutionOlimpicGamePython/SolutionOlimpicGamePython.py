dfeec02a9c05c4ca36375a2283131d430744ceca
UncleAlexKun <raisin27@mail.ru>
2019-11-01 12:52:39 +0400

    Решение задачи D в Яндекс.Интервью
    Только на Python

--- SolutionOlimpicGame/SolutionOlimpicGamePython/SolutionOlimpicGamePython.py
+++ SolutionOlimpicGame/SolutionOlimpicGamePython/SolutionOlimpicGamePython.py
@@ -1,1 +1,11 @@
+def generate(size, result, count_open_brace, count_close_brace):
+    if count_open_brace + count_close_brace == 2 * size:
+        print(result)
+    if count_open_brace < size:
+        generate(size, result + '(', count_open_brace + 1, count_close_brace)
+    if count_open_brace - count_close_brace > 0:
+        generate(size, result + ')', count_open_brace, count_close_brace + 1)
 
+size = int(input())
+output = ''
+generate(size,output, 0, 0)
