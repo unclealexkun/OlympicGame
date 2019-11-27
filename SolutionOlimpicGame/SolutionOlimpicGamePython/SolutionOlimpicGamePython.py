515178655d36dd762dfc47bd3649cd8e0d45c2a2
UncleAlexKun <raisin27@mail.ru>
2019-11-27 22:46:54 +0400

    Финальное решение задачи E

--- SolutionOlimpicGame/SolutionOlimpicGamePython/SolutionOlimpicGamePython.py
+++ SolutionOlimpicGame/SolutionOlimpicGamePython/SolutionOlimpicGamePython.py
@@ -1,20 +1,10 @@
-text_one = input().lower().strip()
-text_two = input().lower().strip()
-
-if len(text_one) != len(text_two) and not(text_one.isalpha or text_two.isalpha):
-    print(0)
+text_one = input()
+text_two = input()
 
 text_one = sorted(text_one)
 text_two = sorted(text_two)
 
-isAnagram = True
-
-for index in range(0, len(text_one)):
-    if text_one[index] != text_two[index]:
-        isAnagram = False
-        break
-
-if isAnagram:
+if text_one == text_two:
     print(1)
 else:
     print(0)
