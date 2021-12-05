6b8d3f059345837debab85a4b17e0dbe3d68f39c
UncleAlexKun <raisin27@mail.ru>
2021-12-05 15:31:16 +0400

    Main function

--- SolutionOlimpicGame/SolutionOlimpicGame/Common.cs
+++ SolutionOlimpicGame/SolutionOlimpicGame/Common.cs
@@ -44,10 +44,146 @@
 		/// Правый разворот дерева.
 		/// </summary>
 		/// <param name="node">Узел</param>
-		/// <returns></returns>
+		/// <returns>Разворот дерева.</returns>
 		public static Node RotateRight(Node node)
 		{
-			
+			var leftNode = node.Left;
+			node.Left = leftNode.Rigth;
+			leftNode.Rigth = node;
+			node.Height = FixHeight(node);
+			leftNode.Height = FixHeight(leftNode);
+
+			return leftNode;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		public static Node RotateLeft(Node node)
+		{
+			var rigthNode = node.Rigth;
+			node.Rigth = rigthNode.Left;
+			rigthNode.Left = node;
+			node.Height = FixHeight(node);
+			rigthNode.Height = FixHeight(rigthNode);
+
+			return rigthNode;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		public static Node Balance(Node node)
+		{
+			node.Height = FixHeight(node);
+
+			if (BalanceFactor(node) == 2)
+			{
+				if (BalanceFactor(node.Rigth) < 0)
+				{
+					node.Rigth = RotateRight(node.Rigth);
+				}
+
+				return RotateLeft(node);
+			}
+
+			if (BalanceFactor(node) == -2)
+			{
+				if (BalanceFactor(node.Left) > 0)
+				{
+					node.Left = RotateLeft(node.Left);
+				}
+
+				return RotateRight(node);
+			}
+
+			return node;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="node"></param>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		public static Node Insert(Node node, int key)
+		{
+			if (node != null)
+			{
+				return new Node(key);
+			}
+
+			if (key < node.Key)
+				node.Left = Insert(node.Left, key);
+			else
+				node.Rigth = Insert(node.Rigth, key);
+
+			return Balance(node);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		public static Node FindMin(Node node)
+		{
+			return node.Left == null
+				? FindMin(node)
+				: node;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		public static Node RemoveMin(Node node)
+		{
+			if (node.Left == null)
+				return node.Rigth;
+			node.Left = RemoveMin(node.Left);
+
+			return Balance(node);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="node"></param>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		public static Node Remove(Node node, int key)
+		{
+			if (node == null)
+				return null;
+
+			if (key < node.Key)
+				node.Left = Remove(node.Left, key);
+			else if (key > node.Key)
+				node.Rigth = Remove(node.Rigth, key);
+			else
+			{
+				var leftNode = node.Left;
+				var rightNode = node.Rigth;
+
+				if (rightNode != null)
+				{
+					return rightNode;
+				}
+
+				var min = FindMin(rightNode);
+				min.Rigth = RemoveMin(rightNode);
+				min.Left = leftNode;
+
+				return Balance(min);
+			}
+
+			return Balance(node);
 		}
 
 		#endregion
